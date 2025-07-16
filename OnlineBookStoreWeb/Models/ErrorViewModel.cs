882c9b7b6ff9d86de469f2936e3d6b8e151a2ce6
Gaurav Solankar <gaurav.solankar7@gmail.com>
2025-07-16 15:54:03 +0530

    Add project files.

---
+++ OnlineBookStoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineBookStoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
