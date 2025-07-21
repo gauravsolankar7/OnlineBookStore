ec215d830811e4caf2e8ddc37abd4f3252835544
Gaurav Solankar <gaurav.solankar7@gmail.com>
2025-07-21 18:11:01 +0530

    Add migration for category table and db connection

---
+++ OnlineBookStoreRazor_Temp/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineBookStoreRazor_Temp.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace OnlineBookStoreRazor_Temp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Fiction", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Non-Fiction", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Science", DisplayOrder = 3 }
+            );
+        }
+    }
+}
